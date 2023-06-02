50f7851845fc874886ab30a80bc3956a7f50eaa6
cnellington <caleb.n.ellington@gmail.com>
2023-06-01 21:38:35 -0700

    add quick formatting util and new words

---
+++ src/constants/formatter.py
@@ -0,0 +1,26 @@
+def get_words(filename):
+    # Processes a file with words on each line and returns a list of lowercase words
+    lines = open(filename, 'r').readlines()
+    words = []
+    seenwords = set()
+    for line in lines:
+        word = line.strip().lower()
+        if word in seenwords or word == '':
+            continue
+        else:
+            words.append(word)
+    return words
+
+def export_words(words, header, outfile):
+    # Export schnoob words
+    outfile = open(outfile, 'w')
+    outfile.write(header + '\n')
+    for word in words:
+        outfile.write(f"  \'{word}\',\n")
+    outfile.write(']')
+
+# Get schnoob words and extra words and write to constants files
+words = get_words('schnoobwords.txt')
+extras = get_words('extrawords.txt')
+export_words(words, 'export const WORDS = [', 'wordlist.ts')
+export_words(words + extras, 'export const VALID_GUESSES = [', 'validGuesses.ts')
